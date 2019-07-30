a515f7eae667c23f7e5e549068db29d51dd95dad
Daniel Frieling <df@plista.com>
2019-07-30 18:14:21 +0200

    improve webpack config wip #fix

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,31 @@ module.exports = {
         extensions: ['', '.js', '.jsx', '.es6'],
         modulesDirectories: ['node_modules']
     },
+    module: {
+        rules: [
+            {
+                test: /\.jade$/,
+                use: 'jade'
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: 'style-loader', // creates style nodes from JS strings
+                    },
+                    {
+                        loader: 'css-loader', // translates CSS into CommonJS
+                    },
+                    {
+                        loader: 'less-loader', // compiles Less to CSS
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './views/home.jade'
+        })
+    ]
 };
